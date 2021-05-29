88b2d747f60149087ebd7b3950813fdbf5a87ad9
GarbyX <60491335+GarbyX@users.noreply.github.com>
2021-05-29 05:56:45 +0100

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+/*
+const myfirstelement = <h1>Hello React!</h1>
+
+ReactDOM.render(myfirstelement, document.getElementById('root'));
+*/
+
+ReactDOM.render(<App />, document.getElementById('root'));
